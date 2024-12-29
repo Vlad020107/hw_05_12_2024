69be1f990e4149b36fe53e39949f682c4e1b741e
Vlad020107 <96010483+Vlad020107@users.noreply.github.com>
2024-12-29 18:17:52 +0300

    Final comit!

--- app/src/main/java/ru/example/a051220242/MainActivity.kt
+++ app/src/main/java/ru/example/a051220242/MainActivity.kt
@@ -2,11 +2,13 @@ package ru.example.a051220242
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -35,5 +37,40 @@ class MainActivity : AppCompatActivity() {
         start_new_act.setOnClickListener {
             startActivity(intent)
         }
+        val buttonSendEmail = findViewById<Button>(R.id.send_mail)
+        buttonSendEmail.setOnClickListener {
+            sendEmail()
+        }
+        val buttonCall = findViewById<Button>(R.id.go_to_ph_num)
+        buttonCall.setOnClickListener {
+            dialPhoneNumber("88005553535")
+        }
     }
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun sendEmail(){
+        val recipient = "mail@yandex.ru"
+        val subject = "Title"
+        val message = "This is text of your mail"
+
+        val intent_mail = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:$recipient")
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT, message)
+        }
+
+        if (intent_mail.resolveActivity(packageManager) != null){
+            startActivity(intent_mail)
+        }
+        else {
+            Toast.makeText(this, "Нет приложений для отправки почты", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    private fun dialPhoneNumber(phoneNumber: String){
+        val intent_phone_num = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        startActivity(intent_phone_num)
+    }
+
 }
